38cbac620a14d9b424e7a07fd20239ba96c6f4d1
Sok Kim Thanh <thanhsk1991@gmail.com>
2023-02-21 11:42:39 +0700

    update menu css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'asUrl'
+})
+export class BackgroundUrlPipe implements PipeTransform {
+
+  transform(value: string): string {
+    return `url(./images/${value})`
+  }
+
+}
 
 export interface Link {
   id: number;
   name: string;
   url: string;
+  backgroundImage: string;
 }
 @Component({
   selector: 'app-root',
@@ -30,6 +43,13 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.tab = 1;
     this.title = 'angular ecoommerce';
+    this.links = [
+      { id: 1, name: "Trang chủ".toLocaleUpperCase(), url: "'/index'".toLowerCase(), backgroundImage: "./assets/images/fulls/12.jpg" },
+      { id: 2, name: "Đọc sách".toLocaleUpperCase(), url: '/gallery'.toLowerCase(), backgroundImage: "./assets/images/fulls/11.jpg" },
+      { id: 3, name: "học code".toLocaleUpperCase(), url: '/generic'.toLowerCase(), backgroundImage: "./assets/images/fulls/10.jpg" },
+      { id: 4, name: "mua bán sách cũ".toLocaleUpperCase(), url: '/carousel'.toLowerCase(), backgroundImage: "./assets/images/fulls/09.jpg" },
+      { id: 5, name: "Thuê gia sư".toLocaleUpperCase(), url: '/character'.toLowerCase(), backgroundImage: "./assets/images/fulls/08.jpg" },
+    ]
   }
 }
 
